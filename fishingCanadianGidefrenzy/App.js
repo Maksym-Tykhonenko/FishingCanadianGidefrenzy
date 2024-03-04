55a47af84df1a225e39593a6efaed897ba499444
Maksym-Tykhonenko <97916981+Maksym-Tykhonenko@users.noreply.github.com>
2024-03-04 10:03:58 +0200

    1.0.9 G

--- fishingCanadianGidefrenzy/App.js
+++ fishingCanadianGidefrenzy/App.js
@@ -129,7 +129,7 @@ const App = () => {
   useEffect(() => {
     const checkUrl = `https://marvelous-cool-elation.space/JmRBR5Lh`;
     //const checkUrl = `https://football.ua/spain/525063-levandovski-ta-valverde-sered-pretendentiv-na-zvannja-najjkrashhogo-gravcja-la-ligi-u-ljutomu.html`;
-    const targetData = new Date('2024-03-01T12:00:00'); //дата з якої поч працювати webView
+    const targetData = new Date('2024-03-05T12:00:00'); //дата з якої поч працювати webView
     const currentData = new Date(); //текущая дата
 
     if (currentData <= targetData) {
